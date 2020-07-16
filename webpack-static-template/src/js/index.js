c98d0258a07b682955d29865fb5605d70de399c6
Yarorslav Yaroshevsky <ameriacanrolling@gmail.com>
2020-07-16 08:34:04 +0700

    перенос проекта в webpack, настройка

--- webpack-static-template/src/js/index.js
+++ webpack-static-template/src/js/index.js
@@ -1,6 +1,7 @@
+import '../scss/style.scss';
 
 import activeNavLink from './src/activeNavLink'
-import openHeight from './src/activeNavLink'
+import openHeight from './src/openHeight'
 import openPopup from './src/openPopup'
 import slider from './src/slider'
 
